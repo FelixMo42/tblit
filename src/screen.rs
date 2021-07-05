3c7cb681df2857aa60c7189000ff410798119faa
FelixMo42 <felix@tacocat.com>
2021-07-05 11:13:45 -0700

    made color traits public

--- src/screen.rs
+++ src/screen.rs
@@ -5,7 +5,7 @@ use termion::{input::{Events, MouseTerminal, TermRead}, raw::{IntoRawMode, RawTe
 type Input = MouseTerminal<AlternateScreen<RawTerminal<Stdout>>>;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
-pub struct Color(u8, u8, u8);
+pub struct Color(pub u8, pub u8, pub u8);
 
 impl Color {
     fn fg(&self) -> String {
@@ -19,7 +19,7 @@ impl Color {
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Style {
-    fg: Color,
+    pub fg: Color,
 }
 
 pub struct Screen {
